c0237b705b78cca3f922fa08965861dfb399adaf
gitgtama <74250079+gitgtama@users.noreply.github.com>
2026-04-12 20:46:26 +0700

    Update dashboard-guest.blade.php

--- resources/views/dashboard-guest.blade.php
+++ resources/views/dashboard-guest.blade.php
@@ -3,7 +3,7 @@
 @section('content')
                 <p class="text-center text-gray-400 mb-6">Welcome</p>
                 <p class="text-center text-gray-400 mb-6">Please login or create an account to continue.</p>
-                <div>
+                <div class="w-full">
                     <a
                         class="w-full bg-blue-500 hover:bg-blue-600 p-3 rounded-lg font-semibold transition"
                         href="{{ secure_route('login') }}"
@@ -12,7 +12,7 @@
                     </a>
                 </div>
                 <br/>
-                <div>
+                <div class="w-full">
                     <a
                         class="w-full bg-green-500 hover:bg-green-600 p-3 rounded-lg font-semibold transition"
                         href="{{ secure_route('register') }}"
